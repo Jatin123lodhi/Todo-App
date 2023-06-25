6dc8456997e0a33affd7f1f4678c67f9f172f0c6
Jatin Lodhi <jatinlodhi111@gmail.com>
2023-06-25 15:05:36 +0530

    todo app created

---
+++ todo-app-be/server.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.json())
+//get all the todos
+app.get("/todos", (req, res) => {
+    fs.readFile("todos.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      res.send(JSON.parse(data));
+    });
+});
+
+//create a new todo
+// let counter = 4;
+app.post("/todos", (req, res) => {
+    const { title, description } = req.body;
+    // const uniqueId = uuid();
+    const uniqueId = Math.floor(Math.random()*100);
+    const newTodo = {
+      title: title,
+      description: description,
+      id: uniqueId,
+    };
+    fs.readFile("todos.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      const todos = JSON.parse(data);
+      todos.push(newTodo);
+      fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+        if (err) throw err;
+        // res.status(201).json(todos)
+        // res.status(201).send({ id: uniqueId });
+        res.status(201).json(todos)
+      });
+    });
+  });
+
+//delete todo
+app.delete("/todos/:id", (req, res) => {
+    const id = req.params.id;
+    console.log("id:",id)
+    let isPresent = false;
+    fs.readFile("todos.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      const todos = JSON.parse(data);
+      for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+          isPresent = true;
+          todos.splice(i, 1);
+          break;
+        }
+      }
+      if (isPresent) {
+        fs.writeFile('todos.json',JSON.stringify(todos),(err)=>{
+          if(err) throw err;
+          res.status(200).json(todos);
+        })
+      } else {
+        console.log(todos)
+        res.sendStatus(404);
+      }
+    });
+  });  
+
+app.listen(5000,()=>{
+    console.log('server started at 5000')
+})
---
+++ todo-app-fe/src/App.js
@@ -0,0 +1,117 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  useEffect(() => {
+    fetch("/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTodos(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //make an api call to create a new todo
+    const resp = await fetch("/todos", {
+      method: "POST",
+      body: JSON.stringify({ title: title, description: description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await resp.json();
+    console.log(data);
+    setTodos(data);
+    setTitle("");
+    setDescription("");
+  };
+
+  const handleDelete = async(e)=>{
+    console.log(e.target.id)
+    const id = e.target.id;
+    const resp = await fetch(`/todos/${id}`,{
+      method:"DELETE",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await resp.json();
+    console.log(data)
+    setTodos(data)
+  }
+  return (
+    <div>
+      <div className="text-3xl font-bold text-blue-400 flex justify-center mt-5">
+        Todo App
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center gap-4 mt-5"
+      >
+        <div>
+          <div className="  text-lg font-semibold text-gray-600">Title</div>
+          <div>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+              className="rounded-lg w-96 p-2 px-4 border border-gray-400 text-gray-700 "
+            />
+          </div>
+        </div>
+        <div>
+          <div className=" text-lg font-semibold text-gray-600">
+            Description
+          </div>
+          <div>
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+              className="rounded-lg w-96 p-2 px-4 border border-gray-400 text-gray-700 "
+            />
+          </div>
+        </div>
+        <button className="p-2 w-96 m-2 bg-blue-400 rounded-lg font-bold text-white">
+          Create a todo
+        </button>
+      </form>
+      <div className="flex flex-col items-center gap-2 m-2 p-2 border-t mt-4">
+        <div className="text-gray-700 font-bold text-lg m-2">
+          List of All the todos
+        </div>
+        <div className="flex  px-4 w-[600px] gap-4 text-gray-600 font-medium hover:bg-gray-200">
+          <div className="py-1 w-[200px] font-bold">ID</div>
+          <div className="py-1 w-[200px] font-bold">Title</div>
+          <div className="py-1 w-[400px] font-bold">Description</div>
+          <div>🗑️</div>
+        </div>
+        {todos.map((todo, idx) => {
+          return (
+            <div
+              key={idx}
+              className="flex  px-4 w-[600px] gap-4 text-gray-600 font-medium hover:bg-gray-200"
+            >
+              <div className="py-1 w-[200px]">{idx + 1}</div>
+              <div className="py-1 w-[200px]">{todo.title}</div>
+              <div className="py-1 w-[400px]">{todo.description}</div>
+              <div id={todo.id} className="cursor-pointer text-xl" onClick={handleDelete}>❎</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
